de8a97e50b989286ccc2a9a365bf00a66dd43450
Temy97 <davidtemyrastaman@gmail.com>
2021-12-15 10:55:38 +0100

    15/dic
    Creadas todas las clases necesarias junto con los metodos y atributos necesarios, no se ejecuta correctamente la app, comprobar ejecucion.

---
+++ Cuenta.java
@@ -0,0 +1,26 @@
+package procesosEJ6;
+
+public class Cuenta {
+	private int saldo;
+
+	public Cuenta(int saldo) {
+		this.saldo = saldo;
+	}
+
+	public void retirarDinero(String nombre, int cantidad) {
+		try {
+			System.out.println(nombre + " va a retirar dinero: " + cantidad + "€ (saldo actual: " + saldo);
+			Thread.sleep(500);
+			if(saldo >= cantidad) {
+				saldo -= cantidad;
+				System.out.println(nombre + " retira -> " + cantidad + " ACTUAL (" + saldo +")");
+
+			}else {
+				System.out.println(nombre + " no puede retirar dinero porque el saldo (" + saldo + ") es menor que la cantidad a retirar (" +  cantidad +")");
+			}
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- Ejecutable.java
+++ Ejecutable.java
@@ -3,8 +3,24 @@ package procesosEJ6;
 public class Ejecutable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Cuenta cuenta = new Cuenta(2000);
+		
+		new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 12; i++) {
+					cuenta.retirarDinero("Agustin", 23);
+				}
+			}
+		};
+		new Runnable() {
+			@Override
+			public void run() {
+				for(int i = 0; i < 17; i++) {
+					cuenta.retirarDinero("Ana", 25);
+				}
+			}
+		};
 	}
 
 }
